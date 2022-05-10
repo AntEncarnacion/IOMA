b345d261651b26d808a4832cc498a1f3b8e94330
Anthony Encarnacion <54995037+AntEncarnacion@users.noreply.github.com>
2022-05-10 18:01:03 -0400

    Update server.py

--- src/p2p/server.py
+++ src/p2p/server.py
@@ -11,35 +11,28 @@ def main():
             data, address = sock.recvfrom(128)
 
             clients = client_join(clients, address, sock)
-            if len(clients) == 2:
-                print('got 2 clients, sending details to each')
-                break
 
-        client_send_info(clients, sock)
-        clients = client_exit(clients, 0, sock)
-        clients = client_exit(clients, 0, sock)
 
 def client_join(clients, address, sock):
-    print('connection from: {}'.format(address))
+    print(f'connection from: {address}')
     clients.append(address)
     sock.sendto(b'ready', address)
-    # client_send_info(clients, sock)
+    client_send_info(clients, sock)
 
     return clients
 
 def client_exit(clients, index, sock):
     clients.pop(index)
-    # client_send_info(clients, sock)
+    client_send_info(clients, sock)
 
     return clients
 
 def client_send_info(clients, sock):
-    # scalable future implementation
-    # encoded_list = format_clients_to_string(clients).encode()
-    # for client in clients:
-    #     sock.sendto(encoded_list, client)
-    sock.sendto('{} {} {}'.format(clients[0][0], clients[0][1], 40000).encode(), clients[1])
-    sock.sendto('{} {} {}'.format(clients[1][0], clients[1][1], 40000).encode(), clients[0])
+    encoded_list = format_clients_to_string(clients).encode()
+    for client in clients:
+        sock.sendto(encoded_list, client)
+    # sock.sendto('{} {} {}'.format(clients[0][0], clients[0][1], 40000).encode(), clients[1])
+    # sock.sendto('{} {} {}'.format(clients[1][0], clients[1][1], 40000).encode(), clients[0])
 
 def format_clients_to_string(clients):
     formatted_list = ''
