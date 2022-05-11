e81ffa811194f3618a3b7ab0ffe693d47d6a81e9
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-05-10 23:08:33 -0400

    add documentation and eliminate code from line 42

--- src/p2p/client.py
+++ src/p2p/client.py
@@ -5,38 +5,51 @@ peer_port=50000
 server_port=40000
 
 def main_client():
+    # Function to get local ip address 
     local_ip=getlocal_ip()
     
+    # Server ip and port (40000)
     server_ip='172.24.84.82'
     rendezvous = (server_ip, server_port)
 
+    # Create a socket and bind it to '0.0.0.0.' and server port
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_sock.bind(('0.0.0.0', server_port))
     
+    # Create a socket and bind it to '0.0.0.0.' and peer port
     peer_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     peer_sock.bind(('0.0.0.0', peer_port))
     
-    
-    data_list_of_peer=connect_to_server(server_sock,rendezvous)    
+    # Connect to the server and receive list of client in format of data string
+    data_list_of_peer=connect_to_server(server_sock,rendezvous)
+
+    # Convert data_list_of_peer to a list of tupple peer_list
     peers_list=convertstr_into_tupple(data_list_of_peer)
+
+    # Print the Peers
     print_peer(peers_list)
+
+    #Punch hole
     punch_hole(peer_sock,peers_list)
+    
+    # Create thread so that the port can listen for server
     listener_server = threading.Thread(target=lambda :listen_server(peers_list,server_sock), daemon=True);
     listener_server.start()
+    
+    # Create thread so that the port can listen for peer
     listener = threading.Thread(target=lambda :listen_peer(peer_sock), daemon=True);
     listener.start()
 
     exit_flag = False
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', server_port))
     while True:
         msg = input('> ')
         if(exit_flag):
-            client_leave(rendezvous,server_sock)
+            client_leave(rendezvous,server_sock) # Leave the chat
             break
-        send_message(peer_sock,msg,peers_list,local_ip)
+        send_message(peer_sock,msg,peers_list,local_ip) # Send message to other peers
     
 def getlocal_ip():
+    # Function to get local ip address
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8",80))
     local_ip=s.getsockname()[0]
@@ -44,7 +57,7 @@ def getlocal_ip():
     return local_ip
 
 def connect_to_server(server_sock,rendezvous):
-      
+    # Connect to the server and receive list of client in format of data string  
     print('connecting to rendezvous server')
     server_sock.sendto(b'join', rendezvous)
     while True:
@@ -58,13 +71,13 @@ def connect_to_server(server_sock,rendezvous):
     return data
    
 def listen_peer(peer_sock):
-    # listen for
+    # listen for peer socket
     while True:
         data = peer_sock.recv(1024)
         print('\rpeer: {}\n> '.format(data.decode()), end='') 
 
 def listen_server(peers_list,server_sock):
-    # listen for
+    # listen for server socket
 
     while True:
         data = server_sock.recv(1024).decode()
@@ -94,7 +107,7 @@ def client_leave(rendezvous,server_sock):
 #         return client[0]        
     
 def print_peer(peers_list):
-        
+    # Print peer list    
         for client in peers_list:
             print('\ngot peer')
             print('  ip:          {}'.format(client[0]))
@@ -102,7 +115,8 @@ def print_peer(peers_list):
             print('  dest port:   {}\n'.format(40000))
 
 def convertstr_into_tupple(data):
-
+   
+    # convert data string into a list of tupple
     decoded_string = data
     li = list(decoded_string.split(" "))
     b=0
